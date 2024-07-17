2c0215edcd93e077bfbff577fbaee54bf2e2390c
Adnan Rashid <adnanrashid1111@gmail.com>
2024-07-17 21:42:48 +0500

    created base page and added bootstrap

--- src/main/java/com/contactmanager/smartcontactmanager/controllers/HomeController.java
+++ src/main/java/com/contactmanager/smartcontactmanager/controllers/HomeController.java
@@ -2,11 +2,10 @@ package com.contactmanager.smartcontactmanager.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.contactmanager.smartcontactmanager.dao.UserRepo;
-import com.contactmanager.smartcontactmanager.entities.User;
 
 @Controller
 public class HomeController {
@@ -14,19 +13,15 @@ public class HomeController {
     @Autowired
     private UserRepo userRepo;
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        User user = new User();
+    @RequestMapping(value = "/home")
+    public String base(Model model) {
+        model.addAttribute("title", "Home - Contact Manager");
+        return "home";
+    }
 
-        user.setName("Adnan Rashid");
-        user.setEmail("adnanrashid111@gmail.com");
-        user.setPassword("12345");
-        user.setRole("user");
-        user.setEnabled(true);
-        user.setImageUrl("asdfsadf.png");
-
-        userRepo.save(user);
-        return "working";
+    @RequestMapping(value = "/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About - Contact Manager");
+        return "about";
     }
 }
