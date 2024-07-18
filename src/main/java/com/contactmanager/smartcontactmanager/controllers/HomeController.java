2fa4db26a6ba9082d7b7a8b2efbba3ad4fb96a43
Adnan Rashid <adnanrashid1111@gmail.com>
2024-07-18 20:28:52 +0500

    completed signedup with data insertion

--- src/main/java/com/contactmanager/smartcontactmanager/controllers/HomeController.java
+++ src/main/java/com/contactmanager/smartcontactmanager/controllers/HomeController.java
@@ -1,11 +1,20 @@
 package com.contactmanager.smartcontactmanager.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.contactmanager.smartcontactmanager.dao.UserRepo;
+import com.contactmanager.smartcontactmanager.entities.User;
+import com.contactmanager.smartcontactmanager.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -27,7 +36,9 @@ public class HomeController {
 
     @RequestMapping(value = "/signuppage")
     public String signup(Model model) {
+        User user = new User();
         model.addAttribute("title", "Signup - Contact Manager");
+        model.addAttribute("user", user);
         return "signuppage";
     }
 
@@ -35,5 +46,34 @@ public class HomeController {
     public String login(Model model) {
         model.addAttribute("title", "Login - Contact Manager");
         return "loginpage";
+    }
+
+    @RequestMapping(value = "/do_register", method = RequestMethod.POST)
+    public String registerUser(@ModelAttribute("user") User user,
+            @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
+            HttpSession session) {
+
+        try {
+            System.out.println(agreement);
+            if (agreement) {
+                System.out.println("You have not agreed the terms and conditions!");
+                throw new Exception("You have not agreed the terms and conditions!");
+            }
+
+            user.setRole("NORMAL");
+            user.setEnabled(true);
+            user.setImageUrl("default.png");
+
+            userRepo.save(user);
+
+            model.addAttribute("user", new User());
+            session.setAttribute("message", new Message("Successfully Registered", "alert-success"));
+            return "signuppage";
+
+        } catch (Exception e) {
+            model.addAttribute("user", user);
+            session.setAttribute("message", new Message("Something went wrong!" + e.getMessage(), "alert-danger"));
+            return "signuppage";
+        }
     }
 }
